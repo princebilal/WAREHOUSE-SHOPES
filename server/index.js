b3b8db725d9ce9a275ebaf2bd48ed6a4710d59dc
princebilal <50054611+princebilal@users.noreply.github.com>
2025-10-24 17:23:02 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -44,8 +44,8 @@ const corsOptions = {
     
     // Add your production domains here
     const allowedOrigins = [
-      'https://www.dwatson.online',
-      'https://dwatson-db-902c7d197f9e.herokuapp.com'
+      'https://shopes-6cd3e7086cdb.herokuapp.com',
+      'https://shopes-6cd3e7086cdb.herokuapp.com/'
     ];
     
     if (allowedOrigins.includes(origin)) {
@@ -2342,6 +2342,7 @@ app.use('*', (req, res) => {
     res.sendFile(path.join(clientDir, 'index.html'));
   }
 });
+
 
 
 
